1f6e85543c865477764e29f6055978af26ac814c
Josh Clyde <joshclyde11@gmail.com>
2017-08-07 20:23:00 -0400

    refactor some

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducers } from './redux/gods';
+import App from './components/App';
+
+const store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/redux/gods/index.jsx
@@ -0,0 +1,10 @@
+const reducers = (state, action) => {
+  if (action) {
+    return state;
+  }
+  return state;
+};
+
+const actions = 'hi';
+
+export { reducers, actions };
